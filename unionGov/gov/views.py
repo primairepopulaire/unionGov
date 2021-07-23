55621b0f336ca3525862c504af2358d0fbbd201e
Olivier Gabriel <olivier@wemaintain.com>
2021-07-23 20:10:46 +0200

    Added Gov view

--- unionGov/gov/views.py
+++ unionGov/gov/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views.generic.list import ListView
 
-from .models import Candidate
+from .models import Candidate, Position
 
+class PositionListView(ListView):
+    model = Position
+    paginate_by = 20
 
-def index(request):
-    candidate_list = Candidate.objects.all()
-    return render(request, 'gov/index.html', {'candidate_list': candidate_list})
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+class CandidateListView(ListView):
+    model = Candidate
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
 
 def candidate(request, candidate_id):
     candidate = get_object_or_404(Candidate, pk=candidate_id)
