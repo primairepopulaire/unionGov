5a07275a5fc2cc9aeaf502c161cd0e955e7bfeb9
Olivier Gabriel <olivier@wemaintain.com>
2021-07-23 14:08:31 +0200

    WIP: working locally

---
+++ unionGov/gov/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.utils.crypto import get_random_string
+
+def get_new_ref():
+    """ Generate a new config ref """
+    return get_random_string(length=32)
+
+# Create your models here.
+class User(models.Model):
+    email_address = models.EmailField()
+    first_name = models.CharField(max_length=32, default="")
+    last_name = models.CharField(max_length=32, default="")
+
+class ConfigRef(models.Model):
+    # long enough random string, to be set at generation
+    config_ref = models.CharField(max_length=32, default=get_new_ref(), unique=True)
+    save_date = models.DateTimeField('date saved')
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return "{} on {}".format(self.config_ref, self.save_date)
+
+class Candidate(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    image_url = models.ImageField()
+
+    def __str__(self) -> str:
+        return "{} {}".format(self.first_name, self.last_name)
+
+class Position(models.Model):
+    position_name = models.CharField(max_length=32)
+
+    def __str__(self) -> str:
+        return self.position_name
+
+class Configs(models.Model):
+    config_ref = models.ForeignKey(ConfigRef, on_delete=models.PROTECT)
+    position = models.ForeignKey(Position, on_delete=models.PROTECT)
+    candidate = models.ForeignKey(Candidate, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return "Config {}: {} pour {}".format(self.config_ref, self.candidate, self.position)
--- unionGov/unionGov/wsgi.py
+++ unionGov/unionGov/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for idealGov project.
+WSGI config for unionGov project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'idealGov.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unionGov.settings')
 
 application = get_wsgi_application()
