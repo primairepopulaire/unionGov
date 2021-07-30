107791a9eceef730a796784accfdc6cb9f31a3ce
Olivier Gabriel <olivier@wemaintain.com>
2021-07-30 17:17:06 +0200

    Updated backend to support front

--- unionGov/gov/views.py
+++ unionGov/gov/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
 
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Candidate, Config, ConfigRef, Position, User
 from .serializers import CandidateSerializer, ConfigSerializer, ConfigRefSerializer 
@@ -13,12 +14,16 @@ class CandidateAPIView(viewsets.ModelViewSet):
     queryset = Candidate.objects.all()
 
 class ConfigAPIView(viewsets.ModelViewSet):
-    serializer_class = ConfigSerializer
     queryset = Config.objects.all()
+    serializer_class = ConfigSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['config_ref']
 
 class ConfigRefAPIView(viewsets.ModelViewSet):
     serializer_class = ConfigRefSerializer
     queryset = ConfigRef.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['config_ref']
 
 class PositionAPIView(viewsets.ModelViewSet):
     serializer_class = PositionSerializer
